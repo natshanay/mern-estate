0a16c561b40f1bb2e5ed42fdd555dc57bb0d92b0
natnael <natnaelchala10@gmail.com>
2026-02-15 23:21:27 -0800

    install react js and tailwind css

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const App = () => {
+  return (
+<div className="bg-blue-500 text-white p-10">
+  If this is blue, Tailwind works
+</div>
+
+  )
+}
+
+export default App
